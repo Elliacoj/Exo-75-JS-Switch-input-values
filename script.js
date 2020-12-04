1fe0123e8e5cd45e1d45e5e4d9d7683a13606e3b
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-04 11:34:58 +0100

    Modif

---
+++ script.js
@@ -0,0 +1,11 @@
+let switcher = function() {
+    let input1 = document.getElementById("input1");
+    let input2 = document.getElementById("input2");
+    let switch1 = input1.value;
+    let switch2 = input2.value;
+    input1.value = switch2;
+    input2.value = switch1;
+
+}
+
+document.getElementById("switch").addEventListener("click", switcher);
